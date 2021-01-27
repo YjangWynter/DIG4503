8577a2fb8bb9eae64b505e051dcb0b467636f978
Yjang Wynter <21028927+YjangWynter@users.noreply.github.com>
2021-01-27 14:47:34 -0400

    Updated lab 3 to actually be cool

--- labs/lab2/index.js
+++ labs/lab2/index.js
@@ -3,11 +3,26 @@ const axios = require('axios');
 const url = 'https://pokeapi.co/api/v2/pokemon/golurk'
 //fetches url then uses takes the response 
 axios(url).then((response)=>{
-    //save the data of the response to a pokemon object
-    const pokemon = response.data;
-    //formatting the name of the pokemon to capitalize the first letter
-    pokemon.name = pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1);
-    //print to the console the pokemon's name and id
-    console.log(`This is a ${pokemon.name} and its ID is #${pokemon.id}!`);
+   format(response.data);
     //if there is an error print to the console
 }).catch((error)=> console.log(`Error ${error}`));
+
+
+function format(pokemon){
+    //formatting the name of the pokemon to capitalize the first letter
+    pokemon.name = pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1);
+    //set a random move
+    let move = randomMove(pokemon);
+    //print to the console the pokemon's name and id and its random move
+    console.log(`Pokemon #${pokemon.id}, ${pokemon.name}, used ${move}!`);
+}
+function randomMove(pokemon){
+    //generate a random number
+    let move_num = Math.floor((Math.random() * pokemon.moves.length) + 1);
+    //select the move with move number
+    let move_name= pokemon.moves[move_num].move.name;
+    //capitalize the move name
+    move_name = move_name.charAt(0).toUpperCase() + move_name.slice(1);
+    //return move
+    return move_name;
+}
