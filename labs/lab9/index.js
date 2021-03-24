74aa17342c5955a86456f3ef64f31b109ce38ab2
YjangWynter <21028927+YjangWynter@users.noreply.github.com>
2021-03-24 11:10:49 -0400

    add changes for browser

---
+++ labs/lab9/index.js
@@ -0,0 +1,38 @@
+import Express from 'express';
+import fs from 'fs';
+
+const App = Express();
+const port = 3003;
+
+let fileContents = fs.readFileSync("database.json");
+
+let database = JSON.parse(fileContents);
+
+App.get("/employees/:name", (req,res) => {
+    let result = {"error": "not found"};
+
+    database.forEach((value)=>{
+        if(req.params.name === value.name){
+            result = value;
+        }
+    });
+    
+    res.json(result);
+});
+
+App.get("/ages/:number", (req,res) => {
+    let result = {"error": "not found"};
+    database.forEach((value)=>{
+        if(parseInt(req.params.number) === value.age){
+            result = value;
+        }
+    });
+    
+    res.json(result);
+});
+App.use("/", Express.static("public"));
+   
+
+App.listen(port, () => {
+    console.log('Server running');
+});
